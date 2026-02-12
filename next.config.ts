a264ec5f904d2bdc642afed14761568b6406a0d9
Aliaksandr Ziabko <aliaksandr.ziabko@celadonsoft.com>
2026-02-12 16:26:32 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,14 @@
 import type { NextConfig } from "next";
 import path from "path";
 
-const nextConfig: NextConfig = {
-  // пустая конфигурация Turbopack
-  turbopack: {},
-
-  webpack: (config) => {
+const nextConfig = {
+  experimental: {
+    turbopack: false,
+  },
+  webpack: (config: any) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
-
-  // явно указываем корень для output tracing
-  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
