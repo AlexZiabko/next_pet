87e810032f78912819198d204c084487a7c3bcfc
Aliaksandr Ziabko <aliaksandr.ziabko@celadonsoft.com>
2026-02-12 15:45:18 +0300

    fix

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+/*
+ *   Copyright (c) 2026 2023 Celadon Development LLC
+ *   All rights reserved.
+ *   Author Aliaksandr Ziabko <aliaksandr.ziabko@celadonsoft.com>
+ */
+export default function Page() {
+  return <p>Invoices Page</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,13 @@
+/*
+ *   Copyright (c) 2026 2023 Celadon Development LLC
+ *   All rights reserved.
+ *   Author Aliaksandr Ziabko <aliaksandr.ziabko@celadonsoft.com>
+ */
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
