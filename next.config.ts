085847ecda8019fd378a7bae9ec7de9afdd42a7b
Aliaksandr Ziabko <aliaksandr.ziabko@celadonsoft.com>
2026-02-12 16:45:54 +0300

    fix: Turbopack + webpack

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
+import path from "path";
 import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    // Настройка алиасов для TypeScript
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+  turbopack: {}, // пустой объект — чтобы Turbopack не ругался
+};
 
 export default nextConfig;
