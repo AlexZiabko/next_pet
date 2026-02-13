3fa03404aacafe4532632a272437a20c6b9f5444
Aliaksandr Ziabko <aliaksandr.ziabko@celadonsoft.com>
2026-02-13 11:30:23 +0300

    Add search

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-/*
- *   Copyright (c) 2026 2023 Celadon Development LLC
- *   All rights reserved.
- *   Author Aliaksandr Ziabko <aliaksandr.ziabko@celadonsoft.com>
- */
-export default function Page() {
-  return <p>Dashboard Page</p>;
-}
